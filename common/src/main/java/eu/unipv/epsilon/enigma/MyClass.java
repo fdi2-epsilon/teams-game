4198b14174a70c84cb0bca952fca0ae74bf97326
Luca Zanussi <luca.z@outlook.com>
2015-03-29 16:42:25 +0200

    Removed dummy class.

--- common/src/main/java/eu/unipv/epsilon/enigma/MyClass.java
+++
@@ -1,9 +0,0 @@
-package eu.unipv.epsilon.enigma;
-
-public class MyClass {
-
-    public String getString() {
-        return "Haloa!";
-    }
-
-}
--- desktop-app/src/main/java/eu/unipv/epsilon/enigma/Application.java
+++ desktop-app/src/main/java/eu/unipv/epsilon/enigma/Application.java
@@ -3,8 +3,7 @@ package eu.unipv.epsilon.enigma;
 public class Application {
 
     public static void main(String[] args) {
-        MyClass a = new MyClass();
-        System.out.println(a.getString());
+        System.out.println("App!");
     }
 
 }
