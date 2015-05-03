41fa440c5f6d81b248fd363aa68dc601faa3ae5d
Luca Zanussi <luca.z@outlook.com>
2015-05-03 17:12:19 +0200

    XML metadata parser + parameterized test
    Some refactoring may be helpful, if we have time

--- common/src/main/java/eu/unipv/epsilon/enigma/loader/levels/parser/XmlMetaParser.java
+++ common/src/main/java/eu/unipv/epsilon/enigma/loader/levels/parser/XmlMetaParser.java
@@ -1,19 +1,135 @@
 package eu.unipv.epsilon.enigma.loader.levels.parser;
 
 import eu.unipv.epsilon.enigma.loader.levels.parser.defaults.DefaultsFactory;
+import eu.unipv.epsilon.enigma.loader.levels.parser.defaults.FieldProvider;
+import eu.unipv.epsilon.enigma.loader.levels.protocol.LevelAssetsURLStreamHandler;
+import eu.unipv.epsilon.enigma.quest.Quest;
 import eu.unipv.epsilon.enigma.quest.QuestCollection;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
 
 public class XmlMetaParser implements MetadataParser {
 
-    public XmlMetaParser(String id, DefaultsFactory defaultsFactory) {
-        // TODO implement
+    public static final String KEYXML_ROOT_NODE = "collection";
+    public static final String KEYXML_QUESTCOLLECTION_META = "meta";
+    public static final String KEYXML_QUESTCOLLECTION_ELEMENTS = "content";
+    public static final String KEYXML_QUEST_NODE = "quest";
+
+    private String collectionId;
+    private DefaultsFactory defaultsFactory;
+
+    public XmlMetaParser(String collectionId, DefaultsFactory defaultsFactory) {
+        this.collectionId = collectionId;
+        this.defaultsFactory = defaultsFactory;
     }
 
     @Override
     public QuestCollection loadCollectionMetadata(InputStream in) {
-        return null;
+        try {
+            DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            return generateCollection(db.parse(in).getDocumentElement());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Cannot parse XML metadata from \"" + collectionId + "\".", e);
+        }
+    }
+
+    private QuestCollection generateCollection(Element document) {
+        QuestCollection qc = new QuestCollection();
+        FieldProvider defaults = defaultsFactory.getCollectionDefaults();
+
+        // Check if root node is <document>...
+        if (!document.getNodeName().equalsIgnoreCase(KEYXML_ROOT_NODE)) {
+            // We may want to handle this gracefully in UI code instead of crashing...
+            throw new RuntimeException("Document should start with \"<" + KEYXML_ROOT_NODE + ">\" root node.");
+        }
+
+        // ...it should contain only one <meta> tag
+        Element meta = getSingleContainerElement(document, KEYXML_QUESTCOLLECTION_META);
+
+        qc.setId(collectionId);
+        qc.setTitle(valueOrDefault(meta, KEY_QUESTCOLLECTION_TITLE, defaults));
+        qc.setSubtitle(valueOrDefault(meta, KEY_QUESTCOLLECTION_SUBTITLE, defaults));
+        qc.setDescription(valueOrDefault(meta, KEY_QUESTCOLLECTION_DESCRIPTION, defaults));
+
+        String iconPathStr = valueOrDefault(meta, KEY_QUESTCOLLECTION_PATH_ICON, defaults);
+        qc.setIconUrl(LevelAssetsURLStreamHandler.createURL(collectionId, iconPathStr));
+
+        // ...and only one <content> tag with <quest> children
+        Element content = getSingleContainerElement(document, KEYXML_QUESTCOLLECTION_ELEMENTS);
+        if (content == null) {
+            // No quests content
+            return qc;
+        }
+
+        NodeList quests = content.getElementsByTagName(KEYXML_QUEST_NODE);
+        for (int i = 0; i < quests.getLength(); ) {
+            if (quests.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                qc.addQuest(generateQuest(i, (Element) quests.item(i)));
+                ++i; // Increment only if correct node type
+            }
+        }
+
+        return qc;
+    }
+
+    private Quest generateQuest(int index, Element meta) {
+        Quest q = new Quest();
+        FieldProvider defaults = defaultsFactory.getQuestDefaults(index);
+
+        q.setName(valueOrDefault(meta, KEY_QUEST_NAME, defaults));
+        q.setDescription(valueOrDefault(meta, KEY_QUEST_DESCRIPTION, defaults));
+
+        Element paths = getSingleContainerElement(meta, KEY_QUEST_PATH_NODE);
+        // Following statements: check if 'paths' node collection exists is inside 'valueOrDefault'
+
+
+        String mainDocPathStr = valueOrDefault(paths, KEY_QUEST_PATH_MAINDOCUMENT, defaults);
+        q.setMainDocumentUrl(LevelAssetsURLStreamHandler.createURL(collectionId, mainDocPathStr));
+
+        String infoDocPathStr = valueOrDefault(paths, KEY_QUEST_PATH_INFODOCUMENT, defaults);
+        q.setInfoDocumentUrl(LevelAssetsURLStreamHandler.createURL(collectionId, infoDocPathStr));
+
+        String iconPathStr = valueOrDefault(paths, KEY_QUEST_PATH_ICON, defaults);
+        q.setIconUrl(LevelAssetsURLStreamHandler.createURL(collectionId, iconPathStr));
+
+        return q;
+    }
+
+    private Element getSingleContainerElement(Element parentNode, String key) {
+        NodeList elements = parentNode.getElementsByTagName(key);
+
+        if (elements.getLength() > 1)
+            throw new RuntimeException("Collection should have only one \"<" + key + ">\" node.");
+
+        // Node type check should be short-circuited if empty check fails, so it should not throw.
+        if (elements.getLength() == 0 || elements.item(0).getNodeType() != Node.ELEMENT_NODE) {
+            // No node or wrong format
+            return null;
+        }
+
+        return (Element) elements.item(0);
+    }
+
+    private String valueOrDefault(Element parentNode, String key, FieldProvider def) {
+        // Handle parent undefined:
+        if (parentNode == null) return def.getPropertyValue(key);
+
+        NodeList nodes = parentNode.getElementsByTagName(key);
+
+        // Handle multiple nodes
+        if (nodes.getLength() > 1)
+            throw new RuntimeException(String.format(
+                    "\"<%s>\" should contain only one \"<%s>\" element.", parentNode.getNodeName(), key));
+        // No nodes
+        if (nodes.getLength() == 0) return def.getPropertyValue(key);
+
+        return nodes.item(0).getTextContent();
     }
 
 }
