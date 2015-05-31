de4a0643e00a0cb1a8fd1f81907f2eac14d4273e
Luca Zanussi <luca.z@outlook.com>
2015-05-31 21:28:24 +0200

    Update RawTemplate to the new API

--- common/src/main/java/eu/unipv/epsilon/enigma/template/builtin/RawTemplate.java
+++ common/src/main/java/eu/unipv/epsilon/enigma/template/builtin/RawTemplate.java
@@ -2,7 +2,6 @@ package eu.unipv.epsilon.enigma.template.builtin;
 
 import eu.unipv.epsilon.enigma.template.api.DocumentGenerationEvent;
 import eu.unipv.epsilon.enigma.template.api.Template;
-import org.w3c.dom.Element;
 
 import java.io.IOException;
 
@@ -32,15 +31,11 @@ public class RawTemplate {
 
     @Template.EventHandler
     public void generate(DocumentGenerationEvent e) throws IOException {
-        Element args = e.getArgumentsRaw();
-
         if (!e.hasPathData())
             throw new UnsupportedOperationException("The \"raw\" template cannot work without path data.");
 
-        Element documentElement = (Element) args.getElementsByTagName("document").item(0);
-        String docPath = documentElement.getAttribute("src");
-
-        e.setResponseStream(e.createRelativePath(docPath).openStream());
+        String documentPath = e.getArguments().query("document:src");
+        e.setResponseStream(e.createRelativePath(documentPath).openStream());
     }
 
 }
